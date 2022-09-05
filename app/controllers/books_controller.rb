c4e05363cc9fddd2ffed6d2fc8acc452c266feef
s-keito <110315888+s-keito@users.noreply.github.com>
2022-09-05 08:38:25 +0000

    [start]app6

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -39,20 +39,7 @@ class BooksController < ApplicationController
     @book.destroy
     redirect_to books_path
   end
-  
-  def self.looks(search, word)
-    if search == "perfect_match"
-      @book = Book.where("title LIKE?","#{word}")
-    elsif search == "forward_match"
-      @book = Book.where("title LIKE?","#{word}%")
-    elsif search == "backward_match"
-      @book = Book.where("title LIKE?","%#{word}")
-    elsif search == "partial_match"
-      @book = Book.where("title LIKE?","%#{word}%")
-    else
-      @book = Book.all
-    end
-  end
+
 
 
   private
